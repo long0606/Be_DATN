2b323f6d374bb7b2e18639211b8457d2117a4e47
long0606 <longlsvnpro@gmail.com>
2022-12-06 21:42:44 +0700

    Bob save

---
+++ router/app.routes.js
@@ -0,0 +1,6 @@
+// const pushNotificationControler = require("../controler/push-notification.controler");
+// const express = require("express");
+// const router = express.Router();
+
+// router.post("/send-notification", pushNotificationControler.sendPushNotification);
+// module.exports=router;
